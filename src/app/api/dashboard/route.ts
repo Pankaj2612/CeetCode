1abfa14fdb7143a8079d3fce37eb4a6e462c1a9e
Pankaj Kumar <sahu94002@gmail.com>
2024-09-16 09:01:47 -0700

    prisma

--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-// Import your data fetching function
 import { gettable } from "../../../../data/table";
 
 export async function GET(request: Request) {
@@ -8,18 +7,20 @@ export async function GET(request: Request) {
   const difficulty = url.searchParams.get("difficulty") || "";
   const tag = url.searchParams.get("tag") || "";
   const searchQuery = url.searchParams.get("searchQuery") || "";
-  // const page = url.searchParams.get("page") || Number;
+  const page = url.searchParams.get("page");
 
   try {
     const questions = await gettable({
       companyName: company,
       difficulty: difficulty,
-      tag: tag,
+      // tag: tag,
       searchQuery: searchQuery,
-      // page: page,
+      page: page ? Number(page) : undefined, // Convert to number if page is provided
     });
+    
     return NextResponse.json(questions);
   } catch (error) {
+    console.error("Error fetching data:", error); // Log the error for debugging
     return NextResponse.json(
       { error: "Failed to fetch data" },
       { status: 500 }
