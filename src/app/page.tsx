6b0395e48de08a364e5498c502b5e020c83f8fe4
Pankaj Kumar <sahu94002@gmail.com>
2024-09-16 08:28:29 -0700

    fixed some bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AllFilter from "@/components/dashboard/filter";
 import { TableDemo } from "@/components/dashboard/question-table";
 
@@ -17,14 +16,4 @@ const Page = () => {
     </div>
   );
 };
-
 export default Page;
-=======
-import { Session } from "inspector/promises";
-import { auth } from "../../auth";
-
-export default async function Home() {
-  const session = await auth();
-  return <div>{JSON.stringify(session)}</div>;
-}
->>>>>>> 0b23b331c4323be05dd72641963d729bfd50fef9
