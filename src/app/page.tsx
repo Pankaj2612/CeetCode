f7672cdf5602da2df85874f97e8d2b979750982c
Pankaj Kumar <sahu94002@gmail.com>
2024-09-17 09:38:12 -0700

    use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+"use client"
 import AllFilter from "@/components/dashboard/filter";
 import { TableDemo } from "@/components/dashboard/question-table";
 import Loader from "@/components/ui/loader";
 import { Suspense } from "react";
 
-const Page = () => {
+const Page = () => { 
   return (
     <Suspense fallback={<Loader/>}>
     <div className="flex flex-col items-center justify-center space-y-4 p-4">
