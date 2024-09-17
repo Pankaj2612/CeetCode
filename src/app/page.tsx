33556da1dd8d4f341d0cfc79382b5bd9553d5737
Pankaj Kumar <sahu94002@gmail.com>
2024-09-17 09:06:21 -0700

    asas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import AllFilter from "@/components/dashboard/filter";
 import { TableDemo } from "@/components/dashboard/question-table";
+import Loader from "@/components/ui/loader";
+import { Suspense } from "react";
 
 const Page = () => {
   return (
+    <Suspense fallback={<Loader/>}>
     <div className="flex flex-col items-center justify-center space-y-4 p-4">
       {/* Filters */}
       <div className="w-full">
@@ -14,6 +17,7 @@ const Page = () => {
         <TableDemo />
       </div>
     </div>
+    </Suspense>
   );
 };
 export default Page;
